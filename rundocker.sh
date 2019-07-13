5825c82772d339c7bcb67e3024ad102abc63b326
Juri Dispan <juri.dispan@me.com>
2019-07-13 17:57:02 +0200

    Docker run skript verbessert

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker run --name sgquote --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g)  ruby:2.5.5-slim ruby sgquote.rb
-
+docker run --name sgquote --rm -v "$(pwd)":/app --workdir /app --user $(id -u):$(id -g)  ruby:2.5.5-slim ruby sgquote.rb "$@"
